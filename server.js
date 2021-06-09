c91b710dc2785a5d6a852728ec69a89831d0f957
aatesh123 <aateshaatesh123@gmail.com>
2021-06-09 08:22:04 +0530

    'intial'

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const ejs=require('ejs')
+const expresslayout=require('express-ejs-layouts')
+const path=require('path')
+const app=express()
+const PORT=process.env.PORT || 3300
+
+app.get('/',(req,res)=>{
+  res.render('home')
+})
+
+app.use(expresslayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
+
+app.listen(PORT,()=>{
+  console.log('listening on port 3300')
+})

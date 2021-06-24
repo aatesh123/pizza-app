5d89321896712b16edb03e3b77cd0bbb3634ce19
aatesh123 <aateshaatesh123@gmail.com>
2021-06-24 07:35:02 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-console.log("aateh tu grta h")
+import axios from 'axios'
+
+const addtocart=document.querySelectorAll('.add-to-cart')
+const cartCounter=document.querySelector('#cartCounter')
+
+function updateCart(pizza){
+  axios.post('/update-cart',pizza).then(res=>{
+    console.log(res)
+    cartCounter.innerText=res.data.totalQty
+    console.log(cartCounter.innerText)
+  })
+}
+addtocart.forEach((btn)=>{
+  btn.addEventListener('click',(e)=>{
+    const pizza=JSON.parse(btn.dataset.pizza)
+    console.log(pizza)
+    updateCart(pizza)
+    })
+})
--- server.js
+++ server.js
@@ -1,11 +1,58 @@
+require('dotenv').config()
 const express=require('express')
 const ejs=require('ejs')
 const expresslayout=require('express-ejs-layouts')
 const path=require('path')
 const app=express()
 const PORT=process.env.PORT || 3300
-
+const mongoose=require('mongoose')
+const session=require('express-session')
+const flash=require('express-flash')
+const MongoDbStore =require('connect-mongo')
+//const =connectmongo(session)
 app.use(express.static('public'))
+app.use(express.json())
+//value capital when class or constuctor
+
+
+//database connection
+
+const url='mongodb://localhost/pizza'
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log('Database connected...')
+}).catch(err => {
+    console.log('Connection failed...')
+})
+// session store
+// let mongostore=new MongoDbStore({
+//   mongooseConnection: connection,
+//   collection: 'sessions'
+// })
+
+
+
+
+///sesssion config use as a middleware
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  store: MongoDbStore.create({
+    mongoUrl: 'mongodb://localhost/pizza'
+  }),
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 1 }
+
+
+}))
+
+app.use((req,res,next)=>{
+  res.locals.session=req.session
+  next()
+})
+
+app.use(flash())
 
 
 
@@ -13,23 +60,10 @@ app.use(expresslayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-  res.render('home')
-})
-
-app.get('/cart',(req,res)=>{
-  res.render('customers/cart')
-})
-
-app.get('/login',(req,res)=>{
-  res.render('auth/login')
-})
-
-app.get('/register',(req,res)=>{
-  res.render('auth/register')
-})
+require('./routes/web')(app)
 
 
+//console.log("kya kya")
 app.listen(PORT,()=>{
   console.log('listening on port 3300')
 })
